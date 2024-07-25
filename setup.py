f8b8767c309142c2844a51241a56cfdbed634d5f
Pavel Bautista <pavelbautista@gmail.com>
2024-07-25 18:45:20 +0200

    Setting the architecture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(name='project-cd',
+      description="package description",
+      packages=["project-cd"])
